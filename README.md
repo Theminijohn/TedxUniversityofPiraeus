5b2eb9bc079b1329811055258a1ae8ff7edc4a2f
The Mini John <admin@phoenix-prime.gr>
2013-11-09 17:59:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is the Official App for [TEDx University of Piraeus](http://www.tedxuniversityofpiraeus.com/)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tedxunipi::Application.config.secret_key_base = '9f3e89df59ba38f82ce0cdae4560038f6cdfe26f095055044943164085c97ba351f5ce937054811b615c7da5babbfdc5a0f8fa3755555ae577cc8bb22395d611'
